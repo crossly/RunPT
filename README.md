cc6a7bff832a6cf821d5c0afd5595d5ecea72708
xlemons <35106813+lemonsX@users.noreply.github.com>
2018-01-04 23:24:56 +0800

    INIT
    A virgin Verison

---
+++ README.md
@@ -0,0 +1,19 @@
+# ProfitTrailer 运行环境一键安装
+本脚本会辅助你安装运行PT所需要的Java8，以及PM2。
+额外做的一件事情是，帮你下载了1.2.6.8的PT，并解压到了/root/ProfitTrailer目录下。
+
+# 特别说明
+运行完这个脚本后，你需要自行通过Winscp修改替换你自己的配置文件和策略文件。
+
+# 环境安装方法：
+  wget -N --no-check-certificate https://github.com/lemonsx/runpt/raw/master/runpt.sh && bash runpt.sh
+
+# 跑PT推荐以下VPS
+
+KVM:
+RAM：512M 一年 18$ https://www.alpharacks.com/myrack/aff.php?aff=1045&pid=58
+RAM：1024M 一年 28$ https://www.alpharacks.com/myrack/aff.php?aff=1045&pid=148
+OpenVZ:
+RAM：768M 一年 9.9$ https://www.alpharacks.com/myrack/aff.php?aff=1045&pid=224 `推荐一个 BOT 买一个这个，挂多个 BOT，买多个。IP 保持唯一。`
+RAM：2048M 一年 19.9$ https://www.alpharacks.com/myrack/aff.php?aff=1045&pid=225
+RAM：3072M 一年 25$ https://www.alpharacks.com/myrack/aff.php?aff=1045&pid=227
---
+++ runpt.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Author:  Xlemon
+# Contact me: Telegram @xlemons
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+clear
+printf "
+#############################################################################################
+#     PT Runtime environment setup for Alpharacks Debian 7&8 Mayby Support Ubunt 14-16      #
+#                 For more information please concact me via Telegram                       #
+#############################################################################################
+"
+# Check if user is root
+[ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
+#update system
+apt-get update
+apt-get install curl build-essential default-jdk software-properties-common -y
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt-get install nodejs
+apt-get update
+apt-get install oracle-java8-installer
+ln -s /usr/bin/nodejs /usr/bin/node
+apt-get install npm
+npm install pm2@latest -g
+
+# Download 1.2.6.8 PT
+wget https://github.com/taniman/profit-trailer/releases/download/v1.2.6.8/ProfitTrailer.zip
+unzip ProfitTrailer.zip
+
+javac -version
+#clean files
+rm -rf nodesource_setup.sh ProfitTrailer.zip ProfitTrailerRuntime.sh
