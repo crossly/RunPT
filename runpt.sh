436e6ae65b6d6ea40410a41045fd70ce66a70b23
xlemons <35106813+lemonsX@users.noreply.github.com>
2018-01-05 22:17:36 +0800

    FIX
    Add unzip
    Add pm2 running config select option

--- runpt.sh
+++ runpt.sh
@@ -13,7 +13,7 @@ printf "
 [ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
 #update system
 apt-get update
-apt-get install curl build-essential default-jdk software-properties-common -y
+apt-get install curl build-essential default-jdk software-properties-common unzip -y
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 bash nodesource_setup.sh
 apt-get install nodejs
@@ -27,6 +27,51 @@ npm install pm2@latest -g
 wget https://github.com/taniman/profit-trailer/releases/download/v1.2.6.8/ProfitTrailer.zip
 unzip ProfitTrailer.zip
 
+# Set VM Ram Usage
+while :; do echo
+  read -p "Are you using Alpharacks' 768RAM VPS ? [y/n]: " alpha_ram
+  if [[ ! $alpha_ram =~ ^[y,n]$ ]]; then
+    echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
+  else
+    break
+  fi
+done
+
+if [ $alpha_ram == 'y' ]; then
+  ram_usage=400
+else
+  ram_usage=512
+fi
+pm2_config_file="/root/ProfitTrailer/pm2-ProfitTrailer.json"
+
+cat > $pm2_config_file <<EOF
+{
+  "apps": [
+    {
+      "name": "profit-trailer",
+      "cwd": ".",
+      "script": "java",
+      "args": [
+        "-XX:+UseConcMarkSweepGC",
+        "-Xmx${ram_usage}m",
+        "-Xms${ram_usage}m",
+        "-jar",
+        "ProfitTrailer.jar"
+      ],
+      "env": {
+        "SPRING_PROFILES_ACTIVE": "prod"
+      },
+      "node_args": [],
+      "log_date_format": "YYYY-MM-DD HH:mm Z",
+      "exec_interpreter": "",
+      "exec_mode": "fork",
+      "autorestart": false
+    }
+  ]
+}
+EOF
+
+# Print java version
 javac -version
-#clean files
+# Clean files
 rm -rf nodesource_setup.sh ProfitTrailer.zip runpt.sh
