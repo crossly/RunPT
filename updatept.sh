2797d4f5bcc072dfb68a5f9d3f61ac444cc68696
xlemons <35106813+lemonsX@users.noreply.github.com>
2018-02-20 21:40:11 +0800

    ROLLBACK

--- updatept.sh
+++ updatept.sh
@@ -13,17 +13,17 @@ apt-get install curl zip -y
 wget https://github.com/taniman/profit-trailer/releases/download/v1.2.6.18/ProfitTrailer.zip
 unzip ProfitTrailer.zip -d /tmp/
 # Start to mod PT
-# unzip /tmp/ProfitTrailer/ProfitTrailer.jar -d /tmp/ProfitTrailer/unzip
-# # add js load script
-# sed '644i    \<script src=\"js/ProfitTrailer_GUI_extensions_v1.1.user.js\"></script>' -i /tmp/ProfitTrailer/unzip/BOOT-INF/classes/templates/index.ftl
+unzip /tmp/ProfitTrailer/ProfitTrailer.jar -d /tmp/ProfitTrailer/unzip
+# add js load script
+sed '644i    \<script src=\"js/ProfitTrailer_GUI_extensions_v1.1.user.js\"></script>' -i /tmp/ProfitTrailer/unzip/BOOT-INF/classes/templates/index.ftl
 
-# pt_plugin_file="/tmp/ProfitTrailer/unzip/BOOT-INF/classes/static/js/ProfitTrailer_GUI_extensions_v1.1.user.js"
+pt_plugin_file="/tmp/ProfitTrailer/unzip/BOOT-INF/classes/static/js/ProfitTrailer_GUI_extensions_v1.1.user.js"
 
-# curl -o /tmp/ProfitTrailer/unzip/BOOT-INF/classes/static/js/ProfitTrailer_GUI_extensions_v1.1.user.js https://raw.githubusercontent.com/lemonsX/RunPT/master/ProfitTrailer_GUI_extensions_v1.1.user.js
-# # Change Folder
-# cd /tmp/ProfitTrailer/unzip
-# # pack jar
-# zip -r -0 ../ProfitTrailer.jar ./*
+curl -o /tmp/ProfitTrailer/unzip/BOOT-INF/classes/static/js/ProfitTrailer_GUI_extensions_v1.1.user.js https://raw.githubusercontent.com/lemonsX/RunPT/master/ProfitTrailer_GUI_extensions_v1.1.user.js
+# Change Folder
+cd /tmp/ProfitTrailer/unzip
+# pack jar
+zip -r -0 ../ProfitTrailer.jar ./*
 mv /tmp/ProfitTrailer/ProfitTrailer.jar /root/ProfitTrailer/ProfitTrailer.jar
 cd ~
 rm -rf /tmp/ProfitTrailer updatept.sh ProfitTrailer.zip
